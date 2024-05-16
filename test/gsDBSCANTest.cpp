7171bb4eb7d921f6a9cb9bc2f3c6d0a0423f3b91
Hugo Phibbs <hugophibbs1@gmail.com>
2024-05-17 11:17:48 +1200

    refactoring gsdbscan tests to use arrayfire instead of eigen arrays

--- test/gsDBSCANTest.cpp
+++ test/gsDBSCANTest.cpp
@@ -3,7 +3,7 @@
 //
 #include <gtest/gtest.h>
 
-#include "../src/dbscan/gsDBSCAN.cpp"
+#include "../include/gsDBSCAN.h"
 #include <Eigen/Dense>
 #include <chrono>
 #include <arrayfire.h>
@@ -13,44 +13,47 @@ class gsDBSCANTest : public ::testing::Test {
 };
 
 TEST_F(gsDBSCANTest, TestFindDistances) {
-    using namespace Eigen;
-    Eigen::MatrixXf X(5, 3);
-    X << 0, 1, 3,
-            1, 2, 0,
-            2, 0, 3,
-            3, 0, 1,
-            0, 0, 1;
+    // Matrix X
+    float dataX[] = {0, 1, 3,
+                     1, 2, 0,
+                     2, 0, 3,
+                     3, 0, 1,
+                     0, 0, 1};
+    af::array X(5, 3, dataX);
 
-    Eigen::MatrixXf A(5, 2);
-    A << 0, 3,
-            2, 5,
-            4, 1,
-            0, 7,
-            2, 1;
+    // Matrix A
+    float dataA[] = {0, 3,
+                     2, 5,
+                     4, 1,
+                     0, 7,
+                     2, 1};
+    af::array A(5, 2, dataA);
 
-    Eigen::MatrixXf B(10, 3);
-    B << 1, 2, 3,
-            0, 4, 1,
-            3, 1, 0,
-            1, 0, 2,
-            0, 2, 3,
-            1, 2, 0,
-            0, 4, 1,
-            3, 1, 2,
-            1, 0, 4,
-            0, 2, 1;
+    // Matrix B
+    float dataB[] = {1, 2, 3,
+                     0, 4, 1,
+                     3, 1, 0,
+                     1, 0, 2,
+                     0, 2, 3,
+                     1, 2, 0,
+                     0, 4, 1,
+                     3, 1, 2,
+                     1, 0, 4,
+                     0, 2, 1};
+    af::array B(10, 3, dataB);
 
-
-    Eigen::MatrixXf initial(5, 6);
-    initial << 11, 5, 14, 11, 0, 5,
-            9, 0, 11, 0, 14, 11,
-            5, 0, 5, 5, 8, 14,
-            9, 5, 0, 0, 9, 5,
-            9, 6, 5, 5, 0, 6;
+    // Matrix initial
+    float dataExpectedDistances[] = {11, 5, 14, 11, 0, 5,
+                                     9, 0, 11, 0, 14, 11,
+                                     5, 0, 5, 5, 8, 14,
+                                     9, 5, 0, 0, 9, 5,
+                                     9, 6, 5, 5, 0, 6};
+    af::array expectedDistances(5, 6, dataExpectedDistances);
 
     auto start = std::chrono::high_resolution_clock::now();
 
+    //distances = findDistances(X, A, B);
 
-
+    // TODO
 
 }
