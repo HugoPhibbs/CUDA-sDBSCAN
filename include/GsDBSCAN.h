16da048042f5754eac453d7d8038359d82a95496
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2024-05-22 11:38:30 +1200

    writing some of the methods from python prototype to cpp code.
    untested though, this is the next ask

--- include/GsDBSCAN.h
+++ include/GsDBSCAN.h
@@ -27,15 +27,15 @@ public:
     GsDBSCAN(const af::array &X, const af::array &D, int minPts, int k, int m, float eps, bool skip_pre_checks);
 
     // Methods corresponding to the functions
-    void performGsDbscan();
+    void performGsDbscan(af::array &X, int D, int minPts, int k, int m, float eps);
 
-    void static preChecks(af::array X, af::array D, int minPts, int k, int m, float eps);
+    void static preChecks(af::array X, int D, int minPts, int k, int m, float eps);
 
     void static preProcessing(af::array D);
 
-    void static randomProjections(af::array X, af::array D, int k, int m, float eps);
+    void static randomProjections(af::array X, int D, int k, int m, float eps);
 
-    void static constructABMatrices(af::array X, af::array D, int k, int m);
+    void static constructABMatrices(af::array X, af::array Y, int k, int m);
 
     af::array static findDistances(af::array X, af::array A, af::array B, float alpha = 1.2);
 
@@ -43,7 +43,9 @@ public:
 
     void constructClusterGraph(af::array distances, float eps, int k, int m);
 
-    void assembleAdjacencyList(af::array distances, int E, int V, af::array A, af::array B, float eps, int blockSize = 1024);
+    af::array GsDBSCAN::assembleAdjacencyList(af::array &distances, af::array &E, af::array &V, af::array &A, af::array &B, float eps, int blockSize=1024);
+
+    af::array constructQueryVectorDegreeArray(af::array distances, float eps);
 
     // Destructor if needed
     ~GsDBSCAN() = default;
