9903f5db98fcc53ecb96d99eaf0f8d95f65980aa
Hugo Phibbs <hugophibbs1@gmail.com>
2024-08-12 11:21:36 +1200

    deleting ccb.sh

--- ccb.sh
+++
@@ -1,7 +0,0 @@
-# Clean Config and Build
-
-rm -rf build && echo "Removed build directory"
-mkdir build && echo "Added build directory"
-
-echo "Configuring build" && cmake . -B build
-echo "Building" && cmake --build build
