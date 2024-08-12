a01df1aba0e0d7871c7a85bf8e912c39136b9814
Hugo Phibbs <hugophibbs1@gmail.com>
2024-08-13 11:50:47 +1200

    Adding tests for converting af array to matx tensor

--- test/gsDBSCAN/UtilsTest.cpp
+++ test/gsDBSCAN/UtilsTest.cpp
@@ -13,12 +13,7 @@ namespace tu = testUtils;
 
 class UtilsTest : public ::testing::Test {
 
-};
-
-class TestColToRowMajorArrayConversion : public UtilsTest {
-
 protected:
-
     template<typename T>
     void assertColRowMajorMatsEqual(T *colMajorArray, T *rowMajorArray, size_t numRows, size_t numCols) {
 
@@ -30,6 +25,16 @@ protected:
                 ASSERT_NEAR(colMajorValue, rowMajorValue, 1e-6);
             }
         }
+    }
+};
+
+class TestColToRowMajorArrayConversion : public UtilsTest {
+
+protected:
+    template <typename T>
+    T* afArrayToHostArray(af::array afArray) {
+        T* deviceArray = afArray.device<float>();
+        return GsDBSCAN::copyDeviceToHost(deviceArray, afArray.elements(), GsDBSCAN::getAfCudaStream());
     }
 };
 
@@ -92,6 +97,62 @@ TEST_F(TestColToRowMajorArrayConversion, TestLargeInput) {
     free(rowMajorMat_h);
 }
 
+class TestArrayFireToMatXConversion : public UtilsTest {
+
+};
+
+TEST_F(TestArrayFireToMatXConversion, TestSmallInput) {
+    const size_t numRows = 3;
+    const size_t numCols = 4;
+
+    float h_colMajorArray[numRows * numCols] = {
+            1.0f, 4.0f, 7.0f, 10.0f,
+            2.0f, 5.0f, 8.0f, 11.0f,
+            3.0f, 6.0f, 9.0f, 12.0f
+    };
+
+    af::array afArray(numRows, numCols, h_colMajorArray);
+
+    auto matXTensor = GsDBSCAN::afArrayToMatXTensor<float, float>(afArray);
+
+    float *matxTensor_d = matXTensor.Data();
+    float *afArray_d = afArray.device<float>();
+
+    auto *matxTensor_h = GsDBSCAN::copyDeviceToHost(matxTensor_d, numRows*numCols, GsDBSCAN::getAfCudaStream());
+    float *afArray_h = GsDBSCAN::copyDeviceToHost(afArray_d, numRows*numCols, GsDBSCAN::getAfCudaStream());
+
+    assertColRowMajorMatsEqual(afArray_h, matxTensor_h, numRows, numCols);
+
+    free(matxTensor_h);
+    free(afArray_h);
+    afArray.unlock();
+}
+
+TEST_F(TestArrayFireToMatXConversion, TestLargeInput) {
+    const int n = 70000;
+    const int d = 1024;
+
+    auto afArray = af::randu(n, d, f32);
+
+    auto start = tu::timeNow();
+
+    auto matXTensor = GsDBSCAN::afArrayToMatXTensor<float, float>(afArray);
+
+    tu::printDurationSinceStart(start);
+
+    float *matxTensor_d = matXTensor.Data();
+    float *afArray_d = afArray.device<float>();
+
+    auto *matxTensor_h = GsDBSCAN::copyDeviceToHost(matxTensor_d, n*d, GsDBSCAN::getAfCudaStream());
+    float *afArray_h = GsDBSCAN::copyDeviceToHost(afArray_d, n*d, GsDBSCAN::getAfCudaStream());
+
+    assertColRowMajorMatsEqual(afArray_h, matxTensor_h, n, d);
+
+    free(matxTensor_h);
+    free(afArray_h);
+    afArray.unlock();
+}
+
 
 class TestEigenToMatXConversion : public UtilsTest {
 
@@ -107,7 +168,6 @@ TEST_F(TestEigenToMatXConversion, TestSmallInput) {
 
     auto matXTensor = GsDBSCAN::eigenMatToMatXTensor<Eigen::half, matx::matxFp16>(halfEigenMat,
                                                                                   matx::MATX_MANAGED_MEMORY);
-
     cudaDeviceSynchronize();
 
     matx::matxFp16 *matXData = matXTensor.Data();
