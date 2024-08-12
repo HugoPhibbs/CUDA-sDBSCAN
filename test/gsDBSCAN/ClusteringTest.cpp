12f47c4f868f37d1ae756d0fc2d22f3f981155f5
Hugo Phibbs <hugophibbs1@gmail.com>
2024-08-12 19:58:53 +1200

    Adding a test for the sort performance in MatX

--- test/gsDBSCAN/ClusteringTest.cpp
+++ test/gsDBSCAN/ClusteringTest.cpp
@@ -28,6 +28,42 @@ class ClusteringTest : public ::testing::Test {
 
 };
 
+class TestSortingProjections : public ClusteringTest{
+
+};
+
+TEST_F(TestSortingProjections, TestSortingProjectionsLargeInputMatX) {
+    // A perf test, doesn't unit test anything
+
+    int n = 20000;
+    int D = 1000;
+
+    auto mockProjections = matx::random<float>({n, D}, matx::UNIFORM) * 100;
+
+    mockProjections.run();
+
+    cudaDeviceSynchronize();
+
+    auto start = tu::timeNow();
+
+    matx::tensor_t<float, 2> projectionsSorted = matx::make_tensor<float>({n, D});
+
+    (projectionsSorted = matx::sort(mockProjections, matx::SORT_DIR_ASC)).run();
+
+    cudaDeviceSynchronize();
+
+    tu::printDurationSinceStart(start);
+
+//    auto projectionsSortedT = matx::sort(matx::transpose(mockProjections), matx::SORT_DIR_ASC);
+//
+//    cudaDeviceSynchronize();
+//
+//    matx::print(projectionsSorted);
+
+
+    // TODO figure out why the sorting is so quick, frankly there is no way it is that quick
+};
+
 
 class TestConstructQueryVectorDegreeArray : public ClusteringTest {
 
@@ -48,6 +84,8 @@ TEST_F(TestConstructQueryVectorDegreeArray, TestSmallInputMatX) {
     auto E = GsDBSCAN::constructQueryVectorDegreeArrayMatx<float>(distances_t, 2.1);
 
     cudaDeviceSynchronize();
+
+    // As as side reference,
 
     // TODO fix why all this nonsense doesn't work
 //
