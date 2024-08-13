7099afe744e43394aa8eae3ae6c82f1b115f1144
Hugo Phibbs <hugophibbs1@gmail.com>
2024-08-13 12:06:42 +1200

    Fixing tests for projections speed with matx and arrayfire.
    Just getting ball park estimations for the later steps

--- test/gsDBSCAN/ProjectionsTest.cpp
+++ test/gsDBSCAN/ProjectionsTest.cpp
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 #include "../../include/gsDBSCAN/GsDBSCAN.h"
 #include "../../include/TestUtils.h"
+#include <cuda_runtime.h>
 
 namespace tu = testUtils;
 
@@ -36,14 +37,35 @@ class TestProjectionsSpeed : public ProjectionsTest {
 
 };
 
-TEST_F(TestProjectionsSpeed, TestLargeInput) {
-//    int n = 70000;
-//    int d = 784;
-//    int D = 1024;
-//
-//    auto X = matx::random<float>({n, d}, matx::UNIFORM);
-//    auto Y = matx::random<float>({d, D}, matx::UNIFORM);
-//    auto Z = matx::matmul(X, Y);
-//
-//    Z.run();
+TEST_F(TestProjectionsSpeed, TestLargeInputArrayFire) {
+    int n = 70000;
+    int d = 784;
+    int D = 1024;
+
+    auto X = af::randu(n, d);
+    auto Y = af::randu(d, D);
+
+    auto start = tu::timeNow();
+    auto Z = af::matmul(X, Y); // Honestly a little too slow!
+    af::eval(Z);
+    cudaDeviceSynchronize(); // Honestly not sure if this is necessary here?
+    tu::printDurationSinceStart(start);
+}
+
+TEST_F(TestProjectionsSpeed, TestLargeInputMatx) {
+    int n = 70000;
+    int d = 784;
+    int D = 1024;
+
+    auto X = matx::random<float>({n, d}, matx::UNIFORM);
+    auto Y = matx::random<float>({d, D}, matx::UNIFORM);
+
+    auto start = tu::timeNow();
+    auto Z = matx::matmul(X, Y);
+    Z.run();
+    cudaDeviceSynchronize(); // 300ms faster than ArrayFire, still, this makes preprocessing slower than CPU.
+    /*
+     * What is needed is a FHT on the GPU. Instead of a simple mat mul.
+     */
+    tu::printDurationSinceStart(start);
 }
