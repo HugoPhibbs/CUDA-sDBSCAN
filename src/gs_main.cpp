74f789746a88923cd88c19f4ef9aca042b1214fe
Hugo Phibbs <hugophibbs1@gmail.com>
2024-08-19 16:58:14 +1200

    running main algo to test integration with a random af array instead of the complete mnist dataset

--- src/gs_main.cpp
+++ src/gs_main.cpp
@@ -6,28 +6,34 @@
 #include <vector>
 
 #include "../include/gsDBSCAN/GsDBSCAN.h"
+#include "cuda_runtime.h"
 #include "../include/gsDBSCAN/utils.h"
+#include <arrayfire.h>
 
 int main() {
 
     std::string XFilename = "/home/hphi344/Documents/Thesis/python/data/mnist_images_col_major.csv";
+//
+//    std::vector<float> X_vec = GsDBSCAN::utils::loadCsvColumnToVector<float>(XFilename, 0);
+//    float *X_h = X_vec.data();
 
-    std::vector<float> X_vec = GsDBSCAN::utils::loadCsvColumnToVector<float>(XFilename, 0);
-    float *X_h = X_vec.data();
-
-    int n = 70000;
-    int d = 784;
+    int n = 1000;
+    int d = 200;
     int k = 5;
-    int m = 50;
-    int D = 1024;
+    int m = 20;
+    int D = 200;
+
+    auto X_af = af::randu(1000, 200, f32);
+
+    X_af.eval();
 
     float eps = 0.11;
     int minPts = 20;
 
-    float *X_d = GsDBSCAN::utils::copyHostToDevice(X_h, n * d);
+//    float *X_d = GsDBSCAN::utils::copyHostToDevice(X_h, n * d);
 
     auto [clusterLabels, typeLabels] = GsDBSCAN::performGsDbscan(
-            X_d,
+            X_af.device<float>(),
             n,
             d,
             D,
@@ -36,4 +42,11 @@ int main() {
             m,
             eps
     );
+
+//    cudaFree(X_d);
+
+
+    X_af.unlock();
+
+    return 0;
 }
