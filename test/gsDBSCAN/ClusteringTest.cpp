587d318ed74cc16f891a1255e4432db5b5602b81
Hugo Phibbs <hugophibbs1@gmail.com>
2024-08-22 16:46:03 +1200

    adding a couple tests for the adjacencylist method

--- test/gsDBSCAN/ClusteringTest.cpp
+++ test/gsDBSCAN/ClusteringTest.cpp
@@ -7,6 +7,7 @@
 #include "../../include/gsDBSCAN/GsDBSCAN.h"
 #include "../../include/TestUtils.h"
 #include <cuda_runtime.h>
+#include <cmath>
 
 namespace tu = testUtils;
 
@@ -206,9 +207,181 @@ TEST_F(TestProcessQueryVectorDegreeArray, TestMnist) {
     tu::printDurationSinceStart(start);
 }
 
+class TestCreatingAdjacencyList : public ClusteringTest {
+
+};
+
+TEST_F(TestCreatingAdjacencyList, TestSmallInput) {
+    int degArray_h[12] = {1, 3, 1, 1, 0, 3, 2, 2, 0, 3, 1, 1};
+    int startIdxArray_h[12] = {0, 1, 4, 5, 6, 6, 9, 11, 13, 13, 16, 17};
+
+    // Assume eps = 3
+    // Just take 4 candidate vectors, this will do for the simple case
+
+    float *distances = {
+
+    };
+
+    // TODO
+}
+
+TEST_F(TestCreatingAdjacencyList, TestSmallInputCase2) {
+    // This example was taken from my python implementation
+
+    int A[10] = {
+            0, 3,
+            2, 5,
+            4, 1,
+            0, 7,
+            2, 1
+    };
+
+    auto A_d = GsDBSCAN::algo_utils::copyHostToDevice(A, 10, false);
+
+    // Flattened integer array for B
+    int B[30] = {
+            1, 2, 3,
+            0, 4, 1,
+            3, 1, 0,
+            1, 0, 2,
+            0, 2, 3,
+            1, 2, 0,
+            0, 4, 1,
+            3, 1, 2,
+            1, 0, 4,
+            0, 2, 1
+    };
+
+    auto B_d = GsDBSCAN::algo_utils::copyHostToDevice(B, 30, false);
+
+
+    float distancesSquared[] = {
+            11.0f,  5.0f, 14.0f, 11.0f,  0.0f,  5.0f,
+            9.0f,  0.0f, 11.0f,  0.0f, 14.0f, 11.0f,
+            5.0f,  0.0f,  5.0f,  5.0f,  8.0f, 14.0f,
+            9.0f,  5.0f,  0.0f,  0.0f,  9.0f,  5.0f,
+            9.0f,  6.0f,  5.0f,  5.0f,  0.0f,  6.0f
+    };
+
+    // Create a new array for the square rooted distances
+    float distances[30];
+
+    // Loop through the distances array and take the square root of each element
+    for (size_t i = 0; i < 30; ++i) {
+        distances[i] = std::sqrt(distancesSquared[i]);
+    }
+
+    auto distances_d = GsDBSCAN::algo_utils::copyHostToDevice(distances, 30, false);
+
+    int degArray[5] = {3, 2, 4, 4, 3};
+    int startIdxArray[5] = {0, 3, 5, 9, 13};
+
+    int *degArray_d = GsDBSCAN::algo_utils::copyHostToDevice(degArray, 5, false);
+    int *startIdxArray_d = GsDBSCAN::algo_utils::copyHostToDevice(startIdxArray, 5, false);
+
+    int n = 5;
+    int k = 1;
+    int m = 3;
+    float eps = std::sqrt(5.1);
+
+    auto [adjacencyList_d, adjacencyList_size] = GsDBSCAN::clustering::constructAdjacencyList(distances_d, degArray_d, startIdxArray_d, A_d, B_d, n, k, m, eps);
+
+    auto adjacencyList_h = GsDBSCAN::algo_utils::copyDeviceToHost(adjacencyList_d, adjacencyList_size);
+
+    int adjacencyListExpected_h[18] {
+            2, 0, 2,
+            1, 1,
+            0, 2, 3, 0,
+            2, 3, 3, 2,
+            0, 0, 4
+    };
+
+    for (int i = 0; i < adjacencyList_size; i++) {
+        ASSERT_EQ(adjacencyListExpected_h[i], adjacencyList_h[i]);
+    }
+}
+
+TEST_F(TestCreatingAdjacencyList, TestSmallInputCase2WithBatching) {
+    int A[10] = {
+            0, 3,
+            2, 5,
+            4, 1,
+            0, 7,
+            2, 1
+    };
+
+    auto A_d = GsDBSCAN::algo_utils::copyHostToDevice(A, 10, false);
+
+    // Flattened integer array for B
+    int B[30] = {
+            1, 2, 3,
+            0, 4, 1,
+            3, 1, 0,
+            1, 0, 2,
+            0, 2, 3,
+            1, 2, 0,
+            0, 4, 1,
+            3, 1, 2,
+            1, 0, 4,
+            0, 2, 1
+    };
+
+    auto B_d = GsDBSCAN::algo_utils::copyHostToDevice(B, 30, false);
+
+
+    float distancesSquared[] = {
+            11.0f,  5.0f, 14.0f, 11.0f,  0.0f,  5.0f,
+            9.0f,  0.0f, 11.0f,  0.0f, 14.0f, 11.0f,
+            5.0f,  0.0f,  5.0f,  5.0f,  8.0f, 14.0f,
+            9.0f,  5.0f,  0.0f,  0.0f,  9.0f,  5.0f,
+            9.0f,  6.0f,  5.0f,  5.0f,  0.0f,  6.0f
+    };
+
+    // Create a new array for the square rooted distances
+    float distances[30];
+
+    // Loop through the distances array and take the square root of each element
+    for (size_t i = 0; i < 30; ++i) {
+        distances[i] = std::sqrt(distancesSquared[i]);
+    }
+
+    auto distances_d = GsDBSCAN::algo_utils::copyHostToDevice(distances, 30, false);
+
+    int degArray[5] = {3, 2, 4, 4, 3};
+    int startIdxArray[5] = {0, 3, 5, 9, 13};
+
+    int *degArray_d = GsDBSCAN::algo_utils::copyHostToDevice(degArray, 5, false);
+    int *startIdxArray_d = GsDBSCAN::algo_utils::copyHostToDevice(startIdxArray, 5, false);
+
+    int n = 5;
+    int k = 1;
+    int m = 3;
+    float eps = std::sqrt(5.1);
+
+    auto [adjacencyList_d, adjacencyList_size] = GsDBSCAN::clustering::constructAdjacencyList(distances_d, degArray_d, startIdxArray_d, A_d, B_d, n, k, m, eps);
+
+    auto adjacencyList_h = GsDBSCAN::algo_utils::copyDeviceToHost(adjacencyList_d, adjacencyList_size);
+
+    int adjacencyListExpected_h[18] {
+            2, 0, 2,
+            1, 1,
+            0, 2, 3, 0,
+            2, 3, 3, 2,
+            0, 0, 4
+    };
+
+    for (int i = 0; i < adjacencyList_size; i++) {
+        ASSERT_EQ(adjacencyListExpected_h[i], adjacencyList_h[i]);
+    }
+}
+
 class TestFormingClusters : public ClusteringTest {
 
 };
+
+TEST_F(TestFormingClusters, TestSmallInputIntegration) {
+
+}
 
 TEST_F(TestFormingClusters, TestSmallInput) {
     // A simple case I came up with from a sketch
@@ -252,7 +425,7 @@ TEST_F(TestFormingClusters, TestSmallInput) {
 
 }
 
-TEST_F(TestFormingClusters, TestSmallInputB) {
+TEST_F(TestFormingClusters, TestSmallInputWithBatches) {
     // A simple case I came up with from a sketch
 
     int n = 12;
