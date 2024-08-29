b211d11cee0be70d1e63aff0e120c63f02551b34
Hugo Phibbs <hugophibbs1@gmail.com>
2024-08-29 17:35:43 +1200

    changing release script to create it's own release build folder

--- scripts/clean_release.sh
+++ scripts/clean_release.sh
@@ -1,9 +1,8 @@
 cd ~/Documents/GS-DBSCAN-CPP
-rm -rf build
-mkdir build
-cd build
+rm -rf build-release
+mkdir build-release
+cd build-release
 cmake .. -DCMAKE_BUILD_TYPE=Release
-cd build
 cmake --build . --config Release -- -j50
 
 echo "Build complete"
