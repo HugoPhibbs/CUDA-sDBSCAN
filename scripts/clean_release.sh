5d8eab5fc1e126fa66203d3471da3008135d1e29
Hugo Phibbs <hugophibbs1@gmail.com>
2024-08-30 08:50:08 +1200

    changing the release script to be specifically for the gs-dbscan script

--- scripts/clean_release.sh
+++ scripts/clean_release.sh
@@ -3,6 +3,6 @@ rm -rf build-release
 mkdir build-release
 cd build-release
 cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --config Release -- -j50
+cmake --build . --target GS-DBSCAN --config Release -- -j50
 
 echo "Build complete"
