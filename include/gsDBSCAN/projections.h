d17623bf9315ba44cae0556c58391598fdcfd223
Hugo Phibbs <hugophibbs1@gmail.com>
2024-08-30 10:03:06 +1200

    constructing AB matrices with torch

--- include/gsDBSCAN/projections.h
+++ include/gsDBSCAN/projections.h
@@ -7,6 +7,7 @@
 
 #include <tuple>
 #include <cmath>
+#include "../pch.h"
 
 #include <arrayfire.h>
 #include "algo_utils.h"
@@ -59,6 +60,55 @@ namespace GsDBSCAN::projections {
         }
 
         return std::tie(A, B);
+    }
+
+    inline std::tuple<torch::Tensor, torch::Tensor> constructABMatricesTorch(const torch::Tensor &projections, int k, int m, const std::string &distanceMetric="L2") {
+        // Assume projections has shape (n, D)
+        int n = projections.size(0);
+        int D = projections.size(1);
+
+        torch::Tensor A = torch::empty({n, 2 * k}, torch::TensorOptions().dtype(torch::kInt32).device(torch::kCUDA));
+        torch::Tensor B = torch::empty({2 * D, m}, torch::TensorOptions().dtype(torch::kInt32).device(torch::kCUDA));
+
+        bool sortDescending;
+
+        if (distanceMetric == "L1" || distanceMetric == "L2") {
+            sortDescending = false;
+        } else if (distanceMetric == "COSINE") {
+            sortDescending = true;
+        } else {
+            throw std::runtime_error("Unknown distanceMetric: '" + distanceMetric + "'");
+        }
+
+        auto dataToRandomIdxSorted = projections.argsort(1, sortDescending).toType(torch::kInt32);
+        auto randomToDataIdxSorted = projections.argsort(0, sortDescending).toType(torch::kInt32);
+
+        auto BEvenIdx = torch::arange(0, 2 * D, 2);
+        auto BOddIdx = BEvenIdx + 1;
+
+        A.slice(1, 0, k) = 2 * dataToRandomIdxSorted.slice(1, 0, k); // Closest
+        A.slice(1, k, 2 * k) = 2 * dataToRandomIdxSorted.slice(1, D - k, D) + 1; // Furthest
+
+        B.index_put_({BEvenIdx, torch::indexing::Ellipsis}, randomToDataIdxSorted.slice(0, 0, m).transpose(0, 1)); // Close -> close
+        B.index_put_({BOddIdx, torch::indexing::Ellipsis}, randomToDataIdxSorted.slice(0, n - m, n).transpose(0, 1)); // Far -> far
+
+        return std::tie(A, B);
+    }
+
+    inline torch::Tensor normaliseDatasetTorch(torch::Tensor X) {
+        auto rowNorms = torch::sqrt(torch::sum(X * X, 1));
+        return X / rowNorms.unsqueeze(1);
+    }
+
+    inline torch::Tensor performProjectionsTorch(torch::Tensor X, int D) {
+        int d = X.size(1);
+        auto Y = torch::randn({d, D});
+        return torch::matmul(X, Y);
+    }
+
+    inline torch::Tensor normaliseAndProjectTorch(torch::Tensor X, int D) {
+        auto X_norm = normaliseDatasetTorch(X);
+        return performProjectionsTorch(X_norm, D);
     }
 
     inline af::array performProjectionsAF(af::array X, int D) {
--- test/ProjectionsTest.cpp
+++ test/ProjectionsTest.cpp
@@ -217,6 +217,61 @@ TEST_F(TestConstructingABMatrices, TestLargeInputAF) {
     ASSERT_TRUE(B_max <= n - 1);
 }
 
+TEST_F(TestConstructingABMatrices, TestSmallInputTorch) {
+    // n = 6, D = 5
+    float distances[30] = {
+            12.0f, 85.0f, 47.0f, 23.0f, 56.0f, 10.0f,
+            63.0f, 77.0f, 20.0f, 34.0f, 89.0f, 4.0f,
+            45.0f, 90.0f, 27.0f, 69.0f, 10.0f, 3.0f,
+            92.0f, 18.0f, 61.0f, 83.0f, 25.0f, 15.0f,
+            51.0f, 39.0f, 74.0f, 6.0f, 81.0f, 2.0f
+    }; // Remember, this is a column major array, so a 6x5 mat
+
+    // A is (n, 2*k), or (6, 2*2) (row major)
+    int expectedA[(6) * (2 * 2)] = {
+            2 * 0, 2 * 2, 2 * 1 + 1, 2 * 3 + 1,
+            2 * 3, 2 * 4, 2 * 0 + 1, 2 * 2 + 1,
+            2 * 1, 2 * 2, 2 * 3 + 1, 2 * 4 + 1,
+            2 * 4, 2 * 0, 2 * 2 + 1, 2 * 3 + 1,
+            2 * 2, 2 * 3, 2 * 4 + 1, 2 * 1 + 1,
+            2 * 4, 2 * 2, 2 * 0 + 1, 2 * 3 + 1
+    };
+
+    // B is (2*D, m), or (2*5, 2) (row major)
+    int expectedB[(2 * 5) * 2] = {
+            5, 0,
+            4, 1,
+            5, 2,
+            1, 4,
+            5, 4,
+            3, 1,
+            5, 1,
+            3, 0,
+            5, 3,
+            2, 4
+    };
+
+    auto distances_d = GsDBSCAN::algo_utils::copyHostToDevice(distances, 30);
+    auto distances_d_row_major = GsDBSCAN::algo_utils::colMajorToRowMajorMat(distances_d, 6, 5);
+
+    auto distancesOptions = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCUDA);
+
+    auto distances_tensor = torch::from_blob(distances_d_row_major, {6, 5}, distancesOptions);
+
+    cudaCheckError();
+
+    auto [A, B] = GsDBSCAN::projections::constructABMatricesTorch(distances_tensor, 2, 2);
+
+    auto A_d = A.mutable_data_ptr<int>();
+    auto B_d = B.mutable_data_ptr<int>();
+
+    auto A_h = GsDBSCAN::algo_utils::copyDeviceToHost(A_d, 6 * 4);
+    auto B_h = GsDBSCAN::algo_utils::copyDeviceToHost(B_d, 10 * 2);
+
+    assertArrayEqual(expectedA, A_h, 6 * 4);
+    assertArrayEqual(expectedB, B_h, 10 * 2);
+}
+
 class TestProjectionsSpeed : public ProjectionsTest {
 
 };
