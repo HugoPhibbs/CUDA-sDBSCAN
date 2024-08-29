34804672d7d4e4e4651207cca95233ad544530dd
Hugo Phibbs <hugophibbs1@gmail.com>
2024-08-30 08:50:58 +1200

    adding a pch folder

---
+++ include/pch.h
@@ -0,0 +1,13 @@
+//
+// Created by hphi344 on 30/08/24.
+//
+
+#ifndef SDBSCAN_PCH_H
+#define SDBSCAN_PCH_H
+
+#include <torch/torch.h>
+#include "lib_include/rapidcsv.h"
+#include "lib_include/json.hpp"
+#include <gtest/gtest.h>
+
+#endif //SDBSCAN_PCH_H
