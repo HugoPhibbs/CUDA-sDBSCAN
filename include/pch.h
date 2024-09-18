679ef48a12ee841cdde94dfe85ea79911318bd71
Hugo Phibbs <hugophibbs1@gmail.com>
2024-09-19 11:29:26 +1200

    removing test from pch

--- include/pch.h
+++ include/pch.h
@@ -9,7 +9,6 @@
 #include "lib_include/rapidcsv.h"
 #include "lib_include/json.hpp"
 #include "lib_include/argparse.hpp"
-#include <gtest/gtest.h>
 #include <matx.h>
 #include "cuda_runtime.h"
 #include <omp.h>
